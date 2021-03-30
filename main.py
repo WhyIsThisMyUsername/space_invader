b813a1b3b1478edee5c9ed381c9b89913adce4a7
Michael <contactheuser@gmail.com>
2021-03-30 14:18:00 +1300

    building button class along with update method - note: update along the middle point by finding half of size of button and passing topleft point to pygame rect. use reference and what you told josh (aka what he wants to murder you about now).

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((640, 480))
 
 bg = pygame.image.load('images/background.jpg')
-
+button_font = pygame.font.SysFont('Corbel', 35)
 
 class Player():
     def __init__(self):
@@ -27,11 +27,25 @@ class Enemy():
 
 class Menus():
     def __init__(self):
-        self.small_button_font = pygame.font.SysFont('Corbel', 35)
+
 
     def main_menu(self):
 
 
+class Button():
+    def __init__(self, text, x, y, bg_color, text_color):
+        self.middle_x = x
+        self.middle_top_y = y
+        self.text_color = text_color
+        self.text = self.set_text(text, bg_color, self.middle_x, self.middle_top_y, self.text_color)
+
+    def set_text(self, text, bg, x, y, text_color):
+        self.text = button_font.render(text, True, pygame.color(text_color))
+        self.text_size = self.text.get_size()
+        self.surface = pygame.Surface(self.text_size)
+        self.surface.fill(bg)
+        self.surface.blit(self.text, (0, 0))
+        self.rect = pygame.Rect(x - self.text_size[0] / 2)
 
 game_running = True
 while game_running:
