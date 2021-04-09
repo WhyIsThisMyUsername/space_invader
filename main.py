c43a8ddc3233324bdbb36cc17d98b012bea5b2cd
WhyIsThisMyUsername <contactheuser@gmail.com>
2021-04-10 10:05:02 +1200

    Created a new resizing text class in the renamed menu_components module (previously button module). Resizing text class is built off the reference, adapted for use in my code. Controls menu has had return button adjusted and placeholder text added.

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from menus import Menus
 pygame.init()
 
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((640, 480), pygame.RESIZABLE)
+screen = pygame.display.set_mode((640, 480))
 
 bg = pygame.image.load('images/background.jpg')
 
 game_running = True
-menu_control = Menus()
+menu_control = Menus(screen)
 menu_control.main_menu()
 status = 'Main Menu'
 
@@ -21,7 +21,7 @@ while game_running:
     clock.tick(60)
     screen.blit(bg, (0, 0))
     # screen.fill('black')
-    menu_control.blit_menu(screen)
+    menu_control.blit_menu()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -30,7 +30,7 @@ while game_running:
         if event.type == pygame.MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed()[0]:
                 if status == 'Main Menu':
-                    button_list = menu_control.to_blit
+                    button_list = menu_control.to_blit_buttons
                     if button_list[0].rect.collidepoint(*pygame.mouse.get_pos()):
                         status = 'Start Game'
                         menu_control.start_game()
--- menus/__init__.py
+++ menus/__init__.py
@@ -1,25 +1,40 @@
 import pygame
-from buttons import Button
+from menu_components import Button
+from menu_components import ResizingText
 
 pygame.init()
 
+large_button_font = pygame.font.SysFont('Corbel', 70)
+medium_button_font = pygame.font.SysFont('Corbel', 35)
+
 
 class Menus:
-    def __init__(self):
-        self.to_blit = []
+    def __init__(self, screen):
+        self.screen = screen
+        self.to_blit_buttons = []
+        self.to_blit_resizing_text = []
     
     def main_menu(self):
-        self.to_blit = []
-        self.to_blit.append(Button('Start', 320, 100, (15, 15, 15), 'white'))
-        self.to_blit.append(Button('Controls', 320, 200, (15, 15, 15), 'white'))
-
-    def controls_menu(self):
-        self.to_blit = []
-        self.to_blit.append(Button('Return', 580, 430, (15, 15, 15), 'white'))
-
-    def start_game(self):
-        pass
+        self.to_blit_buttons = []
+        self.to_blit_resizing_text = []
+        self.to_blit_buttons.append(Button('Start', (320, 100), (15, 15, 15), 'white', large_button_font))
+        self.to_blit_buttons.append(Button('Controls', (320, 200), (15, 15, 15), 'white', large_button_font))
     
-    def blit_menu(self, screen):
-        for i in self.to_blit:
-            screen.blit(i.surface, (i.middle_x, i.middle_top_y))
+    def controls_menu(self):
+        self.to_blit_buttons = []
+        self.to_blit_resizing_text = []
+        controls_text = 'fuck AHHHHH THIS IS TEXT WHY AM I TYPING THIS LOL HELP ME GOD IS DEAD\nTHIS IS A NEW FUCKIN LINE BITCHESSSS'
+        self.to_blit_buttons.append(Button('Return', (580, 430), (15, 15, 15), 'white', medium_button_font))
+        self.to_blit_resizing_text.append(
+            ResizingText(self.screen, controls_text, (50, 50), (620, 540), 'white', (15, 15, 15), medium_button_font))
+    
+    def start_game(self):
+        self.to_blit_buttons = []
+        self.to_blit_resizing_text = []
+    
+    def blit_menu(self):
+        for i in self.to_blit_resizing_text:
+            self.screen.blit(i.new_surface, (0, 0))
+        
+        for i in self.to_blit_buttons:
+            self.screen.blit(i.surface, (i.middle_x, i.middle_top_y))
